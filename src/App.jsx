4c7e9940c906a094376f09ad73f3ae0398c57e7c
khienweoo <devkhinenweoo@gmail.com>
2024-11-07 13:40:32 +0700

    Server and MongoDB setup, built backend APIs

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+import Counter from './components/Counter';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h2>Redux Complete Tutorial</h2>
+
+      </div>
+      
+    </>
+  )
+}
+
+export default App
